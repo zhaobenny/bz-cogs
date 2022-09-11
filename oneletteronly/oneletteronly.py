1779b139480cbfc4b81009f7b4d9854fa0804775
zhaobenny <benny_zhao@sfu.ca>
2022-09-10 18:56:26 -0700

    Revert "bot rename"
    This reverts commit 9ebbf7742a2c972d953740a547bc1dfd6fcae220.

--- oneletteronly/oneletteronly.py
+++ oneletteronly/oneletteronly.py
@@ -9,15 +9,13 @@ class OneLetterOnly(commands.Cog):
     @commands.guild_only()
     @commands.Cog.listener()
     async def on_member_join(self, member: discord.Member):
-        new_nick = ""
         if (member.bot):
-            new_nick = "!"
-
+            return
         # if username => 2 words might as well use it as initals
         words = member.name.split()
         if (len(words) >= 2):
-            new_nick=  new_nick + (words[0][0] + words[1][0]).upper()
+            new_nick= (words[0][0] + words[1][0]).upper()
         else:
-            new_nick =  new_nick + words[0][0].upper()
+            new_nick = words[0][0].upper()
         await member.edit(nick=new_nick)
         return
