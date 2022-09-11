0006dd561b1a3a684c877625cc2b43764475ea60
zhaobenny <benny_zhao@sfu.ca>
2022-09-10 18:53:27 -0700

    update this

--- wherereply/wherereply.py
+++ wherereply/wherereply.py
@@ -1,17 +1,26 @@
 import discord
+from discord.utils import get
 from redbot.core import commands
+
 
 class WhereReply(commands.Cog):
     """Auto-replies to my friends' classic phrase"""
+
     def __init__(self, bot):
         self.bot = bot
+
+    async def sent_idk_msg(self, message):
+        async with message.channel.typing():
+            await message.channel.send(f"idk where is benny, {message.author.display_name}.")
+        return
 
     @commands.guild_only()
     @commands.Cog.listener()
     async def on_message_without_command(self, message: discord.Message):
         if message.author.bot:
             return
-        if message.content.lower() == "where benny":
-            async with message.channel.typing():
-                await message.channel.send("idk where is benny, " + format(message.author.display_name) + ".")
-            return
+        message_text = message.content.lower()
+        if message_text == "where benny":
+            await self.sent_idk_msg(message)
+        elif "where" in message_text and len(message.mentions) == 1 and message.mentions[0].id == 269670939480817664:
+            await self.sent_idk_msg(message)
