fe1eccc75c16e1050ff0603d5e6846af2ec25202
zhaobenny <benny@zhao.gg>
2023-02-18 17:01:44 -0800

    fix this

--- oneletteronly/oneletteronly.py
+++ oneletteronly/oneletteronly.py
@@ -1,21 +1,25 @@
 import discord
 from redbot.core import commands
 
+
 class OneLetterOnly(commands.Cog):
     """Changes new user's nickname to first letter of nickname"""
+
     def __init__(self, bot):
         self.bot = bot
 
     @commands.guild_only()
     @commands.Cog.listener()
     async def on_member_join(self, member: discord.Member):
-        if (member.bot):
-            return
-        # if username => 2 words might as well use it as initals
+
         words = member.name.split()
-        if (len(words) >= 2):
-            new_nick= (words[0][0] + words[1][0]).upper()
+        if (member.bot):
+            new_nick = "!" + words[0][0].upper()
+        elif (len(words) >= 2):
+            new_nick = (words[0][0] + words[1][0]).upper()
         else:
             new_nick = words[0][0].upper()
+
         await member.edit(nick=new_nick)
+
         return
