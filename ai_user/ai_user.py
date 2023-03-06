ba208caa38976d7cf5ed93d05dc147cdbb6554e6
zhaobenny <benny@zhao.gg>
2023-03-05 20:59:12 -0800

    swap to pytesseract for docker support

--- ai_user/ai_user.py
+++ ai_user/ai_user.py
@@ -1,11 +1,11 @@
-from io import BytesIO
 import random
+from io import BytesIO
 
 import discord
 import openai
-from redbot.core import Config, checks, commands
-import easyocr
+import pytesseract
 from PIL import Image
+from redbot.core import Config, checks, commands
 
 
 class AI_User(commands.Cog):
@@ -13,7 +13,7 @@ class AI_User(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-        self.reader = easyocr.Reader(['en'], gpu=False)
+
         self.config = Config.get_conf(self, identifier=754070)
 
         default_global = {
@@ -41,9 +41,9 @@ class AI_User(commands.Cog):
     @ai_user.command()
     @checks.is_owner()
     async def toggle_ocr(self, ctx, value: bool):
-        """ Use CPU to OCR scan images for text to reply to (defaults to False) """
+        """ uses CPU to OCR scan images for text to reply to (defaults to False) """
         await self.config.toggle_ocr.set(value)
-        return await ctx.send("will scan images and reply to them : " + str(value))
+        return await ctx.send(f"[WARNING] Will use up CPU load if enabled, may crash \n Scanning image set to {value}")
 
     @ai_user.command()
     @checks.is_owner()
@@ -86,17 +86,21 @@ class AI_User(commands.Cog):
         if self.whitelist is None:
             self.whitelist = await self.config.guild(message.guild).channels_whitelist()
 
-        if (message.attachments and message.attachments[0] and await self.config.toggle_ocr()):
-            await self.reply_image(message)
-            return
-
-        if (message.channel.id not in self.whitelist or self.skip_reply(message) or
-                (len(message.content.split(" ")) == 1 and (random.random() > 0.5)) or len(message.content) < 5):
+        if (message.channel.id not in self.whitelist or
+            (len(message.content.split(" ")) == 1 and (random.random() > 0.5))
+                or len(message.content) < 5) or message.author.bot:
             return
 
         percent = await self.config.reply_percent()
         if random.random() > percent:
-             return
+            return
+
+        if (message.attachments and message.attachments[0] and await self.config.toggle_ocr()):
+            await self.reply_image(message)
+            return
+
+        if (self.skip_reply(message)):
+            return
 
         return await self.sent_reply(message)
 
@@ -137,31 +141,7 @@ class AI_User(commands.Cog):
         return False
 
     async def reply_image(self, message):
-        """ replies to a image """
-
-        def format_text(arr):
-            arr.sort(key=lambda x: x[0][0][1])
-
-            # Initialize variables to store the current x and y coordinates
-            curr_x = 0
-            curr_y = 0
-
-            # Loop through the sorted array and concatenate the strings in the correct order based on the coordinates
-            result = ''
-            for sub_arr in arr:
-                points = sub_arr[0]
-                text = sub_arr[1]
-                x1, y1 = points[0]
-                x2, y2 = points[2]
-                if y1 == curr_y:
-                    # Concatenate the text to the current line
-                    result += ' ' * (x1 - curr_x) + text
-                else:
-                    # Start a new line and concatenate the text
-                    result += '\n' + ' ' * int(x1) + text
-                curr_x = x2
-                curr_y = y2
-            return " ".join(result.split())
+        """Replies to an image"""
 
         image = message.attachments[0]
         if not (image and image.content_type.startswith('image/')):
@@ -174,34 +154,44 @@ class AI_User(commands.Cog):
         if width > 1500 or height > 2000:  # do not process big images
             return
 
-        result = self.reader.readtext(image_bytes)  # perform OCR on the image
-        if len(result) <= 0:
+        config = '-c tessedit_write_images=False'
+
+        # Perform OCR on the image and get detailed output
+        result = pytesseract.image_to_data(
+            image, output_type=pytesseract.Output.DICT, config=config, timeout=60)
+        text = ""
+        # Filter out words with low confidence scores
+        for i, word in enumerate(result['text']):
+            confidence = int(result['conf'][i])
+            if confidence > 60:
+                text = text + " " + word
+        if not text:
             return
-        result = format_text(result)
-        if len(result) <= 10:
+
+        if len(text) <= 10:
             return
+
         prompt = [
             {"role": "system",
-                     "content": "The following text is from a picture. You are in a Discord text channel. Respond cynically in a short message to the image."},
-            {"role": "user", "content": f"{result}"}
+             "content": "The following text is from a picture. You are in a Discord text channel. Respond cynically in a short message to the image."},
+            {"role": "user", "content": f"{text}"}
         ]
 
         await self.sent_reply(message, prompt=prompt)
-
 
     async def create_messages(self, message: discord.Message):
         """ Create a list of messages to send to OpenAI """
 
         # get the last 10 messages before the current message
-        history= await message.channel.history(limit=10, before=message).flatten()
+        history = await message.channel.history(limit=10, before=message).flatten()
         history.reverse()
 
-        messages= [
+        messages = [
             {"role": "system",
                      "content": "You are in a Discord text channel. Respond to anything, including URLs, unhelpfully and cynically in a short message."},
         ]
 
-        i= 0
+        i = 0
         while (i < len(history)):
             # check if time between messages is more than 20 minutes
             if i > 0 and (history[i].created_at - history[i - 1].created_at).total_seconds() > 1188:
@@ -225,8 +215,8 @@ class AI_User(commands.Cog):
 
     def check_safe_response(self, response):
         """ filters out responses that were moderated out """
-        response= response.lower()
-        filters= ["language model", "openai", "sorry", "please"]
+        response = response.lower()
+        filters = ["language model", "openai", "sorry", "please"]
 
         for filter in filters:
             if filter in response:
