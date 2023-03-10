6891c3b0913f94c2b5534a167b3b25e3a2e6c681
zhaobenny <benny@zhao.gg>
2023-03-09 20:38:28 -0800

    edge cases

--- ai_user/text.py
+++ ai_user/text.py
@@ -3,10 +3,12 @@ import re
 import discord
 
 
-def create_text_prompt(message: discord.Message, bot : discord.Client):
+def create_text_prompt(message: discord.Message, bot: discord.Client):
     # define the regular expression pattern to match URLs
-    pattern = re.compile(r"(https?://\S+)")
-    is_URL = pattern.search(message.content)
+    url_pattern = re.compile(r"(https?://\S+)")
+    is_URL = url_pattern.search(message.content)
+
+    tenor_pattern = r"^https:\/\/tenor\.com\/view\/"
 
     if (not is_URL and not is_acceptable_message(message)):
         return None
@@ -16,17 +18,21 @@ def create_text_prompt(message: discord.Message, bot : discord.Client):
     if is_URL and len(message.embeds) == 0:
         return  # embeds not loaded yet
 
-    if len(message.embeds) > 0:
+    is_tenor = False
+    if (message.embeds):
+        is_tenor = re.match(tenor_pattern, message.embeds[0].url)
+
+    if len(message.embeds) > 0 and not is_tenor:
         prompt = [
             {"role": "system",
              "content": f"You are in a Discord text channel. Respond to anything, including URLs, unhelpfully and cynically in a short message."},
             {"role": "user",
                 "content": f"{message.content}, title is {message.embeds[0].title} and the description is {message.embeds[0].description}"}
         ]
-    elif not is_URL:
+    elif not is_URL or is_tenor:
         prompt = [
             {"role": "system",
-             "content": f"You are {bot.user.name}.  Do not include \"{bot.user.name}:\" in the response. You are in a Discord text channel. Respond to anything unhelpfully and cynically in a short message."},
+             "content": f"You are {bot.user.name}.  Do not include \"{bot.user.name}:\" in the response. You are in a Discord text channel. Respond to anything unhelpfully and cynically, including URLs, in a short message."},
             {"role": "user", "content": message.author.name + ":  " + message.content}
         ]
 
@@ -34,11 +40,17 @@ def create_text_prompt(message: discord.Message, bot : discord.Client):
 
 
 def is_acceptable_message(message: discord.Message):
+
+    if not message.content:
+        return False
+
     if (len(message.content) < 5):
-        print(f"[ai_user] Message too short, skipping: {message.content} in {message.guild.name}")
+        print(
+            f"[ai_user] Message too short, skipping: {message.content} in {message.guild.name}")
         return False
     words = message.content.split()
     if len(words) > 300:
-        print(f"[ai_user] Message too long, skipping: {message.content} in {message.guild.name}")
+        print(
+            f"[ai_user] Message too long, skipping: {message.content} in {message.guild.name}")
         return False
     return True
