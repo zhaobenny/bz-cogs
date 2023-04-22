c4adca887cacd9702ea01af924cb315ee86b3940
zhaobenny <benny@zhao.gg>
2023-04-22 16:35:51 -0700

    add retry on ratelimit

--- ai_user/response/response.py
+++ ai_user/response/response.py
@@ -1,28 +1,42 @@
 import logging
+
 import openai
+import openai.error
+from tenacity import (RetryError, retry, retry_if_exception, stop_after_delay,
+                      wait_random_exponential)
+
 from ai_user.response.checks import is_moderated_response, is_reply
 from ai_user.response.processing import remove_template_from_response
+
 logger = logging.getLogger("red.bz_cogs.ai_user")
 
 
+@retry(retry=(retry_if_exception(openai.error.Timeout) | retry_if_exception(openai.error.APIConnectionError) | retry_if_exception(openai.error.RateLimitError)), wait=wait_random_exponential(min=1, max=10), stop=stop_after_delay(10))
 async def generate_openai_response(model, prompt):
-    try:
-        response = await openai.ChatCompletion.acreate(
-            model=model,
-            messages=prompt,
-        )
-        response = response["choices"][0]["message"]["content"]
-    except:
-        return logger.error(f"Failed API request to OpenAI", exc_info=True)
+    response = await openai.ChatCompletion.acreate(
+        model=model,
+        messages=prompt,
+    )
+    response = response["choices"][0]["message"]["content"]
     return response
 
 
 async def generate_response(message, config, prompt):
     model = await config.model()
 
-    response = await generate_openai_response(model, prompt)
+    try:
+        response = await generate_openai_response(model, prompt)
+    except RetryError:
+        trys = generate_openai_response.retry.statistics["attempt_number"]
+        logger.error(
+            f"Failed {trys} API requests to OpenAI. The last exception was:", exc_info=True)
+        return (False, "😶")
+    except:
+        logger.error(
+            "Failed API request to OpenAI. The exception was:", exc_info=True)
+        return (False, "😶")
 
-    if (not response or (await config.filter_responses()) and is_moderated_response(response, message)):
+    if (await config.filter_responses()) and is_moderated_response(response, message):
         return (False, "😶")
 
     bot_name = message.guild.me.name
