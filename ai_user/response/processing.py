e5e700d56e3b6db7bd9b6508ff4c764eaf4e7374
zhaobenny <benny@zhao.gg>
2023-04-26 11:54:49 -0700

    rm pattern

--- ai_user/response/processing.py
+++ ai_user/response/processing.py
@@ -1,6 +1,10 @@
 import re
 
 def remove_template_from_response(response, bot_name):
-    pattern = r'^User "{}" said:'
-    # remove the User named botname said: from the response if it exists
-    return re.sub(pattern.format(bot_name), '', response)
+    patterns = [
+        r'^User "{}" said:'.format(bot_name),
+        r'^{}:'.format(bot_name),
+    ]
+    for pattern in patterns:
+        response = re.sub(pattern, '', response)
+    return response
