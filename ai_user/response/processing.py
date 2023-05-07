3c62771be7af3b75a166128377012b9543fb170c
Holo <33796679+hollowstrawberry@users.noreply.github.com>
2023-05-07 12:43:50 -0400

    Expand possible template patterns
    To accommodate my observations

--- ai_user/response/processing.py
+++ ai_user/response/processing.py
@@ -2,10 +2,12 @@ import re
 
 def remove_template_from_response(response, bot_name):
     patterns = [
-        r'^User "{}" said:'.format(bot_name),
-        r'^{}:'.format(bot_name),
-        r'^\w+[\"]? said:',
+        rf'^(User )?"?{bot_name}"? (said|says|respond(ed|s)|replie[ds])( to [^":]+)?:?',
+        rf'^As {bot_name}, I( might| would| could)? (respond|reply)( with)?:?',
+        rf'^{bot_name}:',
     ]
     for pattern in patterns:
-        response = re.sub(pattern, '', response)
+        response = re.sub(pattern, '', response).strip(' \n":')
+        if response.count('"') == 1:
+            response = response.replace('"', '')
     return response
