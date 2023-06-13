99596599f9eff44d826da5c1faa05c47c20bc606
zhaobenny <benny@zhao.gg>
2023-06-13 11:38:30 -0700

    gpt-3.5-turbo-16k  support

--- ai_user/common/constants.py
+++ ai_user/common/constants.py
@@ -6,12 +6,15 @@ DEFAULT_REMOVELIST = [
     r'^As an AI language model,'
 ]
 OPENAI_MODEL_TOKEN_LIMIT = {
+    "gpt-4-0613": 7000,
+    "gpt-4-32k-0613": 31000,
+    "gpt-3.5-turbo-16k-0613": 31000,
     "gpt-4": 7000,
     "gpt-4-0314": 7000,
-    "gpt-4-32k": 31000,
-    "gpt-4-32k-0314": 31000,
     "gpt-3.5-turbo": 3000,
     "gpt-3.5-turbo-0301": 3000,
+    "gpt-3.5-turbo-16k": 15000,
+    "gpt-3.5-turbo-0613": 3000
 }
 DEFAULT_REPLY_PERCENT = 0.5
 MIN_MESSAGE_LENGTH = 5
