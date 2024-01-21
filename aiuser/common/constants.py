2ce2a5bf16ffd214b2b4da9695047e135b995d8f
Nineteen <agr19@proton.me>
2024-01-22 00:38:45 +0400

    Update constants.py

--- aiuser/common/constants.py
+++ aiuser/common/constants.py
@@ -38,7 +38,7 @@ DEFAULT_REPLY_PERCENT = 0.5
 
 IMAGE_REQUEST_CHECK_PROMPT = "Your task is to classify messages. You are {botname}. Is the following a message asking for a picture, image, or photo that includes yourself or {botname}?  Answer with True/False."
 IMAGE_REQUEST_SD_GEN_PROMPT = """ I want you to act as an Art Prompt Generator. Maintain maximum accuracy to the original user prompt. You should use Danbooru tags in the prompt.
-Here is a sample of how it should be output: "holding apple, Shopping, Amusement park, sitting on a bench, cozy sweater, autumn park, colorful leaves, 4K quality".
+Here are Danbooru tag examples: "holding apple, Shopping, Amusement park, sitting on a bench, cozy sweater, autumn park, colorful leaves, 4K quality".
 Convert the below message to an Art Prompt.
 The prompt Should be free of the following: line breaks, close-up, dots, delimiters, underscores, photographing, english articles (a, an, and the).
 Keep the prompt as short as possible.
