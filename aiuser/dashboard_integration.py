b9e7e40c3139505a1e356ad1c968c9ea7912abd6
zhaobenny <benny@zhao.gg>
2024-04-30 20:48:18 -0700

    cleanup

--- aiuser/dashboard_integration.py
+++ aiuser/dashboard_integration.py
@@ -32,31 +32,34 @@ class DashboardIntegration(MixinMeta):
         }
 
     @dashboard_page(name="bot_owner_server_config", description="Bot owner specific configuration for a server", methods=("GET", "POST"), is_owner=True)
-    async def send_hello(self, guild: discord.Guild, **kwargs):
+    async def bot_owner_server_config(self, guild: discord.Guild, **kwargs):
         import wtforms
 
         class Form(kwargs["Form"]):
             def __init__(self):
                 super().__init__(prefix="bot_owner_server_config_")
 
-            percent = wtforms.FloatField("General Percent",
+            percent = wtforms.FloatField("Server Response Percent",
                                          validators=[
                                              wtforms.validators.InputRequired(),
-                                             wtforms.validators.NumberRange(min=0, max=10000)
+                                             wtforms.validators.NumberRange(min=0, max=100)
                                          ])
             model: wtforms.SelectFieldBase = wtforms.SelectField(
                 "Model", choices=[])
             whitelist = wtforms.SelectMultipleField(
                 "Whitelisted Channels", choices=[(channel.id, channel.name) for channel in guild.text_channels])
-            reply_to_mentions = wtforms.BooleanField("Always respond to mentions/replies")
+            reply_to_mentions = wtforms.fields.SelectField(
+                "Always respond to mentions/replies", choices=[(True, "True"), (False, "False")])
 
-            submit: wtforms.SubmitField = wtforms.SubmitField("Save")
+            submit: wtforms.SubmitField = wtforms.SubmitField("Save Changes")
 
         form: Form = Form()
 
-        form.reply_to_mentions.default = await self.config.guild(guild).reply_to_mentions_replies()
-
+        reply_mentions = await self.config.guild(guild).reply_to_mentions_replies()
+        form.reply_to_mentions.default = reply_mentions
+        form.reply_to_mentions.data = reply_mentions
         form.percent.default = await self.config.guild(guild).reply_percent() * 100
+        form.whitelist.default = [str(id) for id in await self.config.guild(guild).channels_whitelist()]
 
         models_list = await self.openai_client.models.list()
         if is_using_openai_endpoint(self.openai_client):
@@ -68,16 +71,15 @@ class DashboardIntegration(MixinMeta):
         form.model.default = await self.config.guild(guild).model()
         form.model.choices = [(model, model) for model in models]
 
-        form.whitelist.default = [str(id) for id in await self.config.guild(guild).channels_whitelist()]
-
         if form.validate_on_submit():
             pecentage = form.percent.data
             model = form.model.data
             new_whitelist = [int(id) for id in form.whitelist.data]
-            print(new_whitelist)
+            reply_mentions = form.reply_to_mentions.data
             try:
                 await self.config.guild(guild).reply_percent.set(pecentage / 100)
                 await self.config.guild(guild).model.set(model)
+                await self.config.guild(guild).reply_to_mentions_replies.set(reply_mentions)
                 await self.config.guild(guild).channels_whitelist.set(new_whitelist)
                 self.channels_whitelist[guild.id] = new_whitelist
             except Exception:
