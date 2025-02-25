89b0a629a1d4e0bf917926014378ba3158c82626
zhaobenny <benny@zhao.gg>
2025-02-24 21:03:14 -0800

    refactor: extract default configuration to separate module

--- aiuser/core/aiuser.py
+++ aiuser/core/aiuser.py
@@ -8,20 +8,15 @@ from openai import AsyncOpenAI
 from redbot.core import Config, app_commands, commands
 from redbot.core.bot import Red
 
+from aiuser.config.defaults import DEFAULT_CHANNEL, DEFAULT_GLOBAL, DEFAULT_GUILD, DEFAULT_MEMBER, DEFAULT_ROLE
 from aiuser.core.handlers import handle_message, handle_slash_command
 from aiuser.dashboard_integration import DashboardIntegration
 from aiuser.messages_list.entry import MessageEntry
 from aiuser.core.random_message_task import RandomMessageTask
 from aiuser.settings.base import Settings
 from aiuser.types.abc import CompositeMetaClass
-from aiuser.types.enums import ScanImageMode
 from aiuser.utils.cache import Cache
-from aiuser.config.constants import (
-    DEFAULT_IMAGE_REQUEST_SD_GEN_PROMPT,
-    DEFAULT_IMAGE_REQUEST_TRIGGER_SECOND_PERSON_WORDS,
-    DEFAULT_IMAGE_REQUEST_TRIGGER_WORDS, DEFAULT_IMAGE_UPLOAD_LIMIT,
-    DEFAULT_MIN_MESSAGE_LENGTH, DEFAULT_PRESETS, DEFAULT_RANDOM_PROMPTS,
-    DEFAULT_REMOVE_PATTERNS, DEFAULT_REPLY_PERCENT)
+
 
 from .openai_utils import setup_openai_client
 
@@ -52,77 +47,11 @@ class AIUser(
         self.override_prompt_start_time: dict[int, datetime] = {}
         self.cached_messages: Cache[int, MessageEntry] = Cache(limit=100)
 
-        default_global = {
-            "custom_openai_endpoint": None,
-            "openai_endpoint_request_timeout": 60,
-            "optout": [],
-            "optin": [],
-            "ratelimit_reset": datetime(1990, 1, 1, 0, 1).strftime("%Y-%m-%d %H:%M:%S"),
-            "max_random_prompt_length": 200,
-            "max_prompt_length": 200,
-            "custom_text_prompt": None,
-        }
-
-        default_guild = {
-            "optin_by_default": False,
-            "optin_disable_embed": False,
-            "reply_percent": DEFAULT_REPLY_PERCENT,
-            "messages_backread": 10,
-            "messages_backread_seconds": 60 * 120,
-            "messages_min_length": DEFAULT_MIN_MESSAGE_LENGTH,
-            "reply_to_mentions_replies": True,
-            "scan_images": False,
-            "scan_images_mode": ScanImageMode.AI_HORDE.value,
-            "scan_images_model": "gpt-4o",
-            "max_image_size": DEFAULT_IMAGE_UPLOAD_LIMIT,
-            "model": "gpt-3.5-turbo",
-            "custom_text_prompt": None,
-            "channels_whitelist": [],
-            "roles_whitelist": [],
-            "members_whitelist": [],
-            "public_forget": False,
-            "ignore_regex": None,
-            "removelist_regexes": DEFAULT_REMOVE_PATTERNS,
-            "parameters": None,
-            "weights": None,
-            "random_messages_enabled": False,
-            "random_messages_percent": 0.012,
-            "random_messages_prompts": DEFAULT_RANDOM_PROMPTS,
-            "presets": json.dumps(DEFAULT_PRESETS),
-            "image_requests": False,
-            "image_requests_endpoint": "dall-e-2",
-            "image_requests_parameters": None,
-            "image_requests_sd_gen_prompt": DEFAULT_IMAGE_REQUEST_SD_GEN_PROMPT,
-            "image_requests_preprompt": "",
-            "image_requests_subject": "woman",
-            "image_requests_reduced_llm_calls": False,
-            "image_requests_trigger_words": DEFAULT_IMAGE_REQUEST_TRIGGER_WORDS,
-            "image_requests_second_person_trigger_words": DEFAULT_IMAGE_REQUEST_TRIGGER_SECOND_PERSON_WORDS,
-            "function_calling": False,
-            "function_calling_functions": [],
-            "function_calling_default_location": [49.24966, -123.11934],
-            "conversation_reply_percent": 0,
-            "conversation_reply_time": 20,
-            "custom_model_tokens_limit": None,
-        }
-        default_channel = {
-            "custom_text_prompt": None,
-            "reply_percent": None
-        }
-        default_role = {
-            "custom_text_prompt": None,
-            "reply_percent": None
-        }
-        default_member = {
-            "custom_text_prompt": None,
-            "reply_percent": None
-        }
-
-        self.config.register_member(**default_member)
-        self.config.register_role(**default_role)
-        self.config.register_channel(**default_channel)
-        self.config.register_guild(**default_guild)
-        self.config.register_global(**default_global)
+        self.config.register_member(**DEFAULT_MEMBER)
+        self.config.register_role(**DEFAULT_ROLE)
+        self.config.register_channel(**DEFAULT_CHANNEL)
+        self.config.register_guild(**DEFAULT_GUILD)
+        self.config.register_global(**DEFAULT_GLOBAL)
 
     async def cog_load(self):
         self.openai_client = await setup_openai_client(self.bot, self.config)
