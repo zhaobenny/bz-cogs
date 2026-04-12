d39720173c65125f9a7d690eec0ec3101f396e30
zhaobenny <benny@zhao.gg>
2026-04-12 12:52:48 -0700

    aiuser/aiemote - fix incorrect pydantic versions

--- aiuser/core/aiuser.py
+++ aiuser/core/aiuser.py
@@ -40,7 +40,7 @@ class AIUser(
     Human-like Discord interactions powered by OpenAI (or compatible endpoints) for messages (and images).
     """
 
-    __version__ = "1.7.1"
+    __version__ = "1.7.2"
 
     def __init__(self, bot):
         super().__init__()
